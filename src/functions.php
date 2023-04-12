2a219cb09017210eff0ccef22d83a6a4dbf1b3b9
Steffen Borchmann <steffen.borchmann@tualo.de>
2023-04-12 13:31:03 +0200

    add route pdftotext

--- src/functions.php
+++ src/functions.php
@@ -4,3 +4,4 @@ require_once __DIR__."/Routes/CronRoute.php";
 require_once __DIR__."/Routes/Tesseract.php";
 require_once __DIR__."/Routes/TextAttributes.php";
 require_once __DIR__."/Routes/NewDocument.php";
+require_once __DIR__."/Routes/PdfToText.php";
